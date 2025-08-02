109abc08b83c7b8cb7216aba33671fed7a0f0046
Stefano41Dev <stefano.41.dev@gmail.com>
2025-08-02 13:43:26 -0500

    se añadio el controlador para consultar las categorias

---
+++ ApiTienda/Controllers/ProductoController.cs
@@ -0,0 +1,27 @@
+﻿using ApiTienda.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ApiTienda.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly Tienda2025Context _context;
+
+        public ProductoController(Tienda2025Context context)
+        {   
+            _context = context;
+        }
+
+        [HttpGet("getCategorias")]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias()
+        {
+            return await _context.Categorias.ToListAsync();
+        }
+    }
+    
+}
--- ApiTienda/Models/Tienda2025Context.cs
+++ ApiTienda/Models/Tienda2025Context.cs
@@ -19,10 +19,6 @@ public partial class Tienda2025Context : DbContext
 
     public virtual DbSet<Producto> Productos { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=Tienda2025;Encrypt=False;Integrated Security=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Categoria>(entity =>
